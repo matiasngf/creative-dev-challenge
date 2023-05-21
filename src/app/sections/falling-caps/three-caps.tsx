9e3b0638d202332b5281069db8d175d350f58fee
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2023-05-21 00:05:43 -0300

    migrate three caps

--- src/app/sections/falling-caps/three-caps.tsx
+++ src/app/sections/falling-caps/three-caps.tsx
@@ -2,22 +2,55 @@
 
 import { Center, Float, Resize, useGLTF } from '@react-three/drei'
 import { gsap } from 'gsap'
-import { useEffect, useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import type { Group, Mesh, MeshStandardMaterial } from 'three'
 import type { GLTF } from 'three-stdlib'
 
-import { TrackedDiv, useTrackedGroup } from '~/context/use-tracked'
+import { ThreePortal } from '~/components/common/three-portal'
+import type { TrackerRendererProps } from '~/context/use-tracked-element'
 import { useClientRect } from '~/hooks/use-client-rect'
 
-export const ThreeCaps = () => {
-  const trackedCaps = useTrackedGroup<TrackedDiv>('caps')
+import type { Cap } from '.'
+import s from './caps.module.scss'
+
+export interface CapTrackerProps {
+  cap: Cap
+}
+
+interface CapPortalProps {
+  cap: Cap
+  element: HTMLDivElement | null
+}
+
+export const CapTracker = ({ cap }: CapTrackerProps) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  const [capProps, setCapProps] = useState<CapPortalProps>({
+    cap,
+    element: ref.current
+  })
+
+  useEffect(() => {
+    setCapProps((s) => ({
+      ...s,
+      element: ref.current
+    }))
+  }, [])
 
   return (
-    <group>
-      {trackedCaps.map((cap) => (
-        <ThreeCap key={cap.id} {...cap} />
-      ))}
-    </group>
+    <div
+      ref={ref}
+      className={s.cap}
+      style={{
+        left: cap.left,
+        top: cap.top,
+        right: cap.right,
+        bottom: cap.bottom,
+        transform: `scale(${cap.scale})`
+      }}
+    >
+      <ThreePortal props={capProps} autoAdd renderer={CapPortal} />
+    </div>
   )
 }
 
@@ -36,8 +69,13 @@ interface CapGLTF extends GLTF {
 
 useGLTF.preload('/models/cap.glb')
 
-export const ThreeCap = ({ el }: TrackedDiv) => {
+export const CapPortal = ({
+  props
+}: TrackerRendererProps<CapPortalProps, undefined>) => {
   const { nodes } = useGLTF('/models/cap.glb') as CapGLTF
+
+  const { element } = props
+  const rect = useClientRect(element)
 
   const SceneNode = useMemo(() => {
     const Scene = nodes.Capv2.clone(true)
@@ -45,14 +83,19 @@ export const ThreeCap = ({ el }: TrackedDiv) => {
   }, [nodes.Capv2])
 
   const groupRef = useRef<THREE.Group>(null)
+  const randomY = useMemo(() => (Math.random() - 0.5) * Math.PI * 0.2 - 0.3, [])
+  const randomX = useMemo(() => (Math.random() - 0.5) * Math.PI * 0.1 + 0.2, [])
+  const randomZ = useMemo(() => (Math.random() - 0.5) * Math.PI * 0.05, [])
 
   useEffect(() => {
     if (!groupRef.current) return
 
     const triggerEl = document.getElementById('#capsContainer')
+    // const randomRotation = Math.random() * Math.PI * 2
 
     gsap.to(groupRef.current.rotation, {
-      y: Math.PI * -2,
+      x: randomX,
+      y: randomY,
       scrollTrigger: {
         scrub: true,
         trigger: triggerEl,
@@ -60,9 +103,7 @@ export const ThreeCap = ({ el }: TrackedDiv) => {
         end: 'bottom bottom'
       }
     })
-  }, [])
-
-  const rect = useClientRect(el)
+  }, [randomY, randomX])
 
   const centerX = rect.absoluteLeft + rect.width / 2
   const centerY = rect.absoluteTop + rect.height / 2
@@ -72,10 +113,10 @@ export const ThreeCap = ({ el }: TrackedDiv) => {
     <group
       scale={[scaleFactor, scaleFactor, scaleFactor]}
       position={[centerX, -centerY, 500]}
-      rotation={[-0.1, 0, 0]}
+      rotation={[randomX + 0.3, randomY + 0.3, randomZ]}
       ref={groupRef}
     >
-      <Float rotationIntensity={1} floatIntensity={0.3}>
+      <Float>
         <Center>
           <Resize>
             <primitive object={SceneNode} />
